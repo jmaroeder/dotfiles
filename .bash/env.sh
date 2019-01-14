6bf600ac500ca0c9cdb66508362985f78ea47647
James Allen <james.m.allen@gmail.com>
2019-01-14 13:36:15 -0500

    Blank slate

--- .bash/env.sh
+++
@@ -1,2 +0,0 @@
-# environment variable exports go here, e.g.
-# export ENV_VARIABLE='foo'
--- .bash/venv.sh
+++
@@ -1,6 +0,0 @@
-# pip should only run if there is a virtualenv currently activated
-export PIP_REQUIRE_VIRTUALENV=true
-
-gpip(){
-   PIP_REQUIRE_VIRTUALENV="" pip "$@"
-}
